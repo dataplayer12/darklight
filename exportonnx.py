80ff2f8530fa700fb1631a8e07923f0a82cf27bd
jaiyam <jaiyam_sharma@yanmar.com>
2022-09-01 01:30:10 +0900

    export onnx with given batch size

--- exportonnx.py
+++ exportonnx.py
@@ -1,13 +1,19 @@
 import torch
 import os
 import torchvision.models as models
+import sys
+
+
+BATCHSIZE=128 if len(sys.argv)==1 else int(sys.argv[1])
+
+print('Using batch size: ', BATCHSIZE)
 
 net=models.resnet152(pretrained=True)
 
-x=torch.randn(1,3,224,224)
+x=torch.randn(BATCHSIZE,3,224,224)
 
 inputn=['img']
 outputn=['probs']
 #dynamic_axes = {'img': {0: 'bsize'}, 'probs': {0: 'bsize'}}
 
-torch.onnx.export(net, x, 'resnet152.onnx',input_names=inputn, output_names=outputn)#, dynamic_axes=dynamic_axes)
+torch.onnx.export(net, x, 'resnet152_bs{}.onnx'.format(BATCHSIZE),input_names=inputn, output_names=outputn)#, dynamic_axes=dynamic_axes)
