a3010005d0586f895683882bda1b2caf19fef560
jaiyam <jaiyam_sharma@yanmar.com>
2022-09-01 01:32:28 +0900

    verified high accuracy in TRT

--- testtrt.py
+++ testtrt.py
@@ -2,23 +2,24 @@ import trtinfer
 import sys
 import torch
 import torchvision.models as models
-sys.path.insert(0, '../simple_vit/')
 
-tvmodel=models.resnet152(pretrained=True)
+BATCHSIZE=128 if len(sys.argv)==1 else int(sys.argv[1])
+
+tvmodel=models.resnet152(pretrained=True).eval()
 
 clsr=trtinfer.TRTClassifier(
-		'resnet152.onnx',
+		'resnet152_bs{}.onnx'.format(BATCHSIZE),
 		nclasses=1000,
 		insize=(224,224),
 		imgchannels=3,
-		maxworkspace=(1<<25), 
+		maxworkspace=(1<<27), 
 		precision='FP32', 
 		device='GPU', 
-		max_batch_size=1
+		max_batch_size=128
 		)
 
 from dataset import ImageNetManager
-dm=ImageNetManager('/sfnvme/imagenet/',size=[224,224] ,bsize=1)
+dm=ImageNetManager('/sfnvme/imagenet/',size=[224,224] ,bsize=BATCHSIZE)
 
 it=iter(dm.train_loader)
 
@@ -26,23 +27,27 @@ x,y = next(it)
 #x=x.cuda()
 #y=y.cuda()
 
-print(clsr.infer(x, benchmark=True, transfer=True))
+#print(clsr.infer(x, benchmark=True, transfer=True))
 
 ix=0
 for x,y in it:
 	#x=x.cuda()
 	#y=y.cuda()
-	clsr.infer(x, benchmark=True, transfer=True)
+	#sh=torch.cuda.current_stream()
+	clsr.infer(x, benchmark=True, transfer=True)#, sh=sh)
 	trtout=clsr.output.argmax(axis=1)
-	print(clsr.output)
+	#print('accuracy=',(trtout == y.numpy()).sum())
 	with torch.no_grad():
-		tvout=tvmodel(x).numpy()
-		print(tvout)
-		tvout=tvout.argmax(axis=1)
+		tvout=tvmodel(x)
+		trtp=torch.nn.functional.softmax(torch.tensor(clsr.output), dim=1)
+		tvp=torch.nn.functional.softmax(tvout)
+		diff=abs(trtp-tvp).sum(axis=1).mean().item()
+		tvout=tvout.numpy().argmax(axis=1)
 
 	racc= (trtout==tvout).sum()
 	aacc = (trtout == y.numpy()).sum()
-	print(racc, aacc)
+	
+	print(racc, aacc, diff)
 	ix+=1
 	if ix==10:
 		print(tvout.shape)
