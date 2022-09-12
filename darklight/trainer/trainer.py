98e3232eec79636d4a5a1afd43dce41de5b43bd1
jaiyam <jaiyam_sharma@yanmar.com>
2022-09-12 23:24:53 +0900

    relatice import of trtengine and losses

--- darklight/trainer/trainer.py
+++ darklight/trainer/trainer.py
@@ -7,59 +7,94 @@ from torch.utils.tensorboard import SummaryWriter
 import os
 import pdb
 import sys
-
+from ..losses.losses import SoftLabelsDistillationLoss, HardLabelDistillationLoss 
+from ..trtengine.clsengine import TRTClassifier
 try:
 	from apex import amp
+	has_amp=True
 except ImportError:
-	print('AMP not found')
+	print('AMP not found, mixed precision training not available')
+	has_amp=False
 
 class StudentTrainer(object):
-	def __init__(self, net, dm, teacher_onnx=None):
+	def __init__(self, net, dm, teacher_onnx=None, opt_params=None):
 		'''
-		net: student net to train
+		net: (nn.Module) student net to train
+		
 		dm: data manager class as defined in ImageNetManager
-		teacher_onnx: path of onnx file for tescher network,
-		use utils.exportonnx to export onnx file
-		If teacher path is None, dark knowledge is not use
+		
+		teacher_onnx: (str) path of onnx file for teacher network,
+			use utils.exportonnx to export onnx file
+			If teacher path is None, dark knowledge is not used
+		
+		opt_params: (dict) 
+		{
+			'optimizer': optimizer caller,
+			'okwargs': keyword arguments to pass to optimizer constructor
+			'scheduler': scheduler caller,
+			'skwargs': keyword arguments to pass to init scheduler,
+			'amplevel': None or 'O1' or 'O2'. If None, amp is not used
+		}
+
+		If opt_params is None, AdamW is used with a constant learning rate of 1e-4
 		'''
 		self.use_dark_knowledge=True if teacher_onnx is not None else False
 		self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 		self.net=net.to(self.device)
 		self.dm=dm
 		self.writer=SummaryWriter()
-		self.criterion=SoftLabelsLoss(10) if self.use_dark_knowledge else nn.CrossEntropyLoss()
-		self.optimizer=optim.AdamW(self.net.parameters(), lr=1e-5, weight_decay=0.05)
-		self.scheduler=torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-			self.optimizer,
-			T_0=2,
-			T_mult=2
-			)
+		self.criterion=SoftLabelsDistillationLoss(10) if self.use_dark_knowledge else nn.CrossEntropyLoss()
+
+		if opt_params is None:
+			self.optimizer=optim.AdamW(self.net.parameters(), lr=1e-4, weight_decay=0.05)
+			self.has_scheduler= False #scheduler is not None
+			self.amplevel=None
+		else:
+			self.optimizer=opt_params['optimizer'](self.net.parameters(), **opt_params['okwargs'])
+			self.scheduler=opt_params['scheduler'](self.optimizer, **opt_params['skwargs'])
+			self.amplevel=opt_params['amplevel']
+			self.has_scheduler= True
+			#torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+			#	self.optimizer,
+			#	T_0=2,
+			#	T_mult=2
+			#	)
 		self.trtengine=None
 		self.savepath=None
 		self.get_accuracy=lambda p,y: (torch.argmax(p, dim=1) == y).to(torch.float).mean().item()
 
 		if self.use_dark_knowledge:
 			self.trtengine=TRTClassifier(
-			'resnet152_bs{}.onnx'.format(BATCH_SIZE),
+			teacher_onnx,
 			nclasses=1000,
-			insize=(224,224),
+			insize=self.dm.size,
 			imgchannels=3,
-			maxworkspace=(1<<27), 
-			precision='FP16', 
-			device='GPU', 
-			max_batch_size=BATCH_SIZE
+			maxworkspace=(1<<27),
+			precision='FP16',
+			device='GPU',
+			max_batch_size=self.dm.bsize
 			)
 
-	def resume_training(self, loadpath, epochs, save, sstep=0):
+	def resume_training(self, loadpath, epochs, save, rstep=0):
+		'''
+		loadpath: (str) checkpoint path to resume from
+		epochs: (int) number of epochs to train
+		save: (str) savepath to format
+		rstep: (int) step number to resume from (reflected in tensorboard logs)
+		'''
 		self.net.load_state_dict(torch.load(loadpath))
-		self.train(epochs, save, sstep)
+		self.train(epochs, save, rstep)
 
 	def evaluate_model(self, step, verbose=False):
+		'''
+		step: (int) step to write in tensorboard log
+		verbose: (bool) whether or not to print performance statistics
+		'''
 		self.net.eval()
 		valoss=[]
 		vaacc=[]
 		with torch.no_grad():
-			pass
+			
 			for imgs, ys in self.dm.valid_loader:
 				imgs=imgs.to(self.device)
 				ys=ys.to(self.device)
@@ -86,8 +121,9 @@ class StudentTrainer(object):
 
 		self.net.to(self.device).train()
 
-		# self.net, self.optimizer = amp.initialize(self.net, self.optimizer,
-		# 							opt_level='O2', enabled=True)
+		if has_amp and self.amplevel is not None:
+			self.net, self.optimizer = amp.initialize(self.net, self.optimizer,
+										opt_level=self.amplevel, enabled=True)
 
 		step=rstep
 		
@@ -116,13 +152,17 @@ class StudentTrainer(object):
 				
 				self.writer.add_scalar('Training Loss', loss.item(), step)
 
-				# with amp.scale_loss(loss, self.optimizer) as scaled_loss:
-				# 	scaled_loss.backward()
-
-				loss.backward()
+				if has_amp and self.amplevel is not None:
+					with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+						scaled_loss.backward()
+				else:
+					loss.backward()
 
 				self.optimizer.step()
-				self.scheduler.step(epoch + ix/nbatches)
+
+				if self.has_scheduler:
+					self.scheduler.step(epoch + ix/nbatches)
+
 				acc=self.get_accuracy(pred, y)
 				step+=1
 				self.writer.add_scalar('Training Accuracy', acc, step)
@@ -167,7 +207,7 @@ if __name__=="__main__":
 	stream=torch.cuda.Stream()
 
 	with torch.cuda.stream(stream):
-		trainer=CookTrainer(net, dm)
+		trainer=StudentTrainer(net, dm)
 		print('Created trainer')
 		
 		trainer.resume_training('mbv2_dk_try2/mbv2_dk_in1k_14.pth', 45, 'mbv2_dk_in1k_{}.pth', sstep=750000)
